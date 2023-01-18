c883337f6296edc623e1c100bf5bfb0e2ccd7ce3
Marcin Jaborski <marcin.jaborski@gmail.com>
2023-01-18 21:02:21 +0100

    Better car settings

--- src/Components/CarSettings.tsx
+++ src/Components/CarSettings.tsx
@@ -1,17 +1,45 @@
 import { Box, Divider, Slider } from "@mui/material";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import { useState } from "react";
 
 const CarSettings = () => {
+  const [temperature, setTemperature] = useState(33);
   return (
     <Box
       sx={{
         display: "flex",
-        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         height: "100%",
       }}
     >
+      <Box sx={{ height: 300, mr: 3 }}>
+        <ThermostatIcon fontSize="large" />
+        <Slider
+          orientation="vertical"
+          value={temperature}
+          onChange={(event, value) => setTemperature(value as number)}
+          sx={{
+            color:
+              temperature < 33
+                ? "cyan"
+                : temperature < 66
+                ? "orange"
+                : "crimson",
+            mt: 3,
+            ".MuiSlider-markLabel": {
+              color: "white",
+            },
+          }}
+          marks={[
+            { value: 0, label: "16°C" },
+            { value: 33, label: "19°C" },
+            { value: 66, label: "22°C" },
+            { value: 100, label: "25°C" },
+          ]}
+        />
+      </Box>
       <Box className="big-button">
         <img
           src="windshield.png"
@@ -28,7 +56,32 @@ const CarSettings = () => {
           />
         </Box>
       </Box>
-      <Slider defaultValue={30} sx={{ color: "crimson", mt: 3 }} />
+      <Box sx={{ height: 300, mr: 3 }}>
+        <Slider
+          orientation="vertical"
+          defaultValue={50}
+          sx={{
+            ml: 3,
+            mt: 3,
+            ".MuiSlider-markLabel": {
+              color: "white",
+            },
+          }}
+          step={33}
+          marks={[
+            { value: 0, label: "off" },
+            { value: 33, label: "1" },
+            { value: 66, label: "2" },
+            { value: 100, label: "3" },
+          ]}
+        />
+        <img
+          src="air.png"
+          alt="fan"
+          width={35}
+          style={{ filter: "invert(1)" }}
+        />
+      </Box>
     </Box>
   );
 };
